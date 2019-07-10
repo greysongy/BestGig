d973b31a8ff413c422f8f40be4d81a1446bde07a
blueDogs <danielchicchon@gmail.com>
2019-07-10 12:33:05 -0700

    add inputs to home.html and take those inputs to post in home.js

--- routes/userData-api-routes.js
+++ routes/userData-api-routes.js
@@ -2,7 +2,12 @@ var db = require("../models");
 
 module.exports = function(app) {
     app.get("/api/userData", function(req,res) {
-        db.userData.findAll({}).then(function(dbuserData) {
+        db.UserData.findAll({}).then(function(dbUserData) {
+            res.json(dbUserData)
+        })
+    })
+    app.post("/api/userData", function(req, res) {
+        db.userData.create(req.body).then(function(dbuserData) {
             res.json(dbuserData)
         })
     })
