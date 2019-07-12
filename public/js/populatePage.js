8dcb0d0939fec3cb5a391cfa9743d737aee33cc5
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-12 16:51:09 -0700

    second functionality commit

--- public/js/populatePage.js
+++ public/js/populatePage.js
@@ -57,26 +57,26 @@ $(document).ready(function () {
 
 
     // Here we look at the route /api/companies to find data for companies
-    $.get("/api/companies", function (data) {
-        companies = data;
-        console.log(companies)
-        if (data) {
-            company = companies[0]
-            companyName = company.company_name.toString()
-            companyRating = company.average_rating.toString()
-            companyPay = company.average_pay_per_hour.toString()
-            companyReviews = company.number_reviews.toString()
-            companyLocation = company.location.toString()
+    // $.get("/api/companies", function (data) {
+    //     companies = data;
+    //     console.log(companies)
+    //     if (data) {
+    //         company = companies[0]
+    //         companyName = company.company_name.toString()
+    //         companyRating = company.average_rating.toString()
+    //         companyPay = company.average_pay_per_hour.toString()
+    //         companyReviews = company.number_reviews.toString()
+    //         companyLocation = company.location.toString()
 
-            $("#company-name").text(companyName)
-            $("#average-rating").text(companyRating)
-            $("#average-pay").text(companyPay)
-            $("#number-reviews").text(companyReviews)
-            $("#company-location").text(companyLocation)
-        } else {
-            console.log("No data to show! To see already created tables, remove force: true in server.js towards the bottom of the page")
-        }
+    //         $("#company-name").text(companyName)
+    //         $("#average-rating").text(companyRating)
+    //         $("#average-pay").text(companyPay)
+    //         $("#number-reviews").text(companyReviews)
+    //         $("#company-location").text(companyLocation)
+    //     } else {
+    //         console.log("No data to show! To see already created tables, remove force: true in server.js towards the bottom of the page")
+    //     }
 
 
-    })
+    // })
 })
--- public/js/userReview.js
+++ public/js/userReview.js
@@ -1,3 +1,5 @@
+// var db = require("../../../models");
+
 $(document).ready(function() {
     var nameInput = $("#name");
     var companyInput = $("#company");
@@ -5,5 +7,72 @@ $(document).ready(function() {
     var payInput = $("#pay");
     var locationInput = $("#location");
 
-    
+    $("#submitReview").on("click", function(event) {
+        console.log("Did we get here");
+        event.preventDefault();
+        var newReview = {
+            company_name: companyInput.val().trim(), 
+            username: nameInput.val().trim(), 
+            pay_per_hour: parseFloat(payInput.val().trim()), 
+            rating: parseInt(ratingInput.val().trim(), 10), 
+            location: locationInput.val().trim()
+        };
+
+        console.log("New Review");
+        console.log(newReview);
+
+        //could do something here to query company database and check if one exists with compan
+        //information we'd need to do all of this here: we'd need the models,
+        // var queriedCompany = db.companies.findAll({
+        //     where: {
+        //         company_name: newReview.company_name, 
+        //         location: newReview.location
+        //     }
+        // });
+        // if(queriedCompany === null) {
+        //     db.companies.create({
+        //         company_name: newReview.company_name, 
+        //         average_rating: newReview.rating, 
+        //         average_pay_per_hour: newReview.pay_per_hour, 
+        //         number_reviews: 1, 
+        //         location: newReview.location
+        //     }).then(function (dbCompany) {
+        //         res.json(dbCompany)
+        //     });
+        // }
+
+        // else {
+        //     var currentRating = queriedCompany.average_rating;
+        //     var currentPay = queriedCompany.average_pay_per_hour;
+        //     var currentNumRatings = queriedCompany.number_reviews;
+        //     var newNumRatings = currentNumRatings++;
+        //     var newPay = (currentPay + newReview.pay_per_hour)/newNumRatings;
+        //     var newRating = (currentRating + newReview.rating)/newNumRatings;
+        //     db.companies.update({
+        //         average_rating: newRating, 
+        //         average_pay_per_hour: newPay, 
+        //         number_reviews: newNumRatings
+        //     }, {
+        //         where: {
+        //             company_name: newReview.company_name, 
+        //             location: newReview.location
+        //         }
+        //     }).then(function (dbCompany) {
+        //         res.json(dbCompany)
+        //     })
+        // }
+
+        $.ajax({
+            method: "PUT",
+            url: "/api/companies",
+            data: newReview
+        })
+            .then(function () {
+                window.location.href = "/test";
+            })
+
+        $.post("/api/reviews", newReview, function() {
+            console.log("Sent new Review");
+        })
+    })
 })
