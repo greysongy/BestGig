ea9c1d6426e280888a800b48e6ab971dace74410
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-07-13 23:28:07 -0700

    Added code to sort companies by pay

--- public/js/newUser.js
+++ public/js/newUser.js
@@ -35,10 +35,52 @@ $(document).ready(function () {
             $.get("/api/companies/" + companyLocation).then(function (dbCompanies) {
                 console.log("List of companies");
                 console.log(dbCompanies)
-                // Here is where we sort by pay
+                // Here is where we sort by pay, greyson: I'm thinking we create a new array and then use that for the rest of the calculations
 
-                for (var i = 0; i < dbCompanies.length; i++) {
-                    var splitName = dbCompanies[i].company_name.toLowerCase().split(" ").join().replace(/,/, "");
+                var newdbCompanies;
+                console.log("Mergesort Result");
+                console.log(mergeSort(dbCompanies));
+                var sortedCompanies = mergeSort(dbCompanies);
+                console.log("Did we get here");
+
+                //sorted in opposite direction
+                function mergeSort(arr) {
+                    if (arr.length < 2)
+                        return arr;
+
+                    var middle = parseInt(arr.length / 2);
+                    var left = arr.slice(0, middle);
+                    var right = arr.slice(middle, arr.length);
+
+                    return merge(mergeSort(left), mergeSort(right));
+                }
+
+                function merge(left, right) {
+                    var result = [];
+
+                    while (left.length && right.length) {
+                        if (left[0].average_pay_per_hour <= right[0].average_pay_per_hour) {
+                            result.push(left.shift());
+                        } else {
+                            result.push(right.shift());
+                        }
+                    }
+
+                    while (left.length)
+                        result.push(left.shift());
+
+                    while (right.length)
+                        result.push(right.shift());
+
+                    return result;
+                }
+
+                for (var i = sortedCompanies.length - 1; i >= 0; i--) {
+                    //code to format name when it's made up of multiple words
+                    console.log("Current company");
+                    console.log("I: " + i);
+                    console.log(sortedCompanies[i]);
+                    var splitName = sortedCompanies[i].company_name.toLowerCase().split(" ").join().replace(/,/, "");
                     console.log("rearranged name");
                     console.log(splitName);
                     $("#results").append(`<div class="row mt-5">
@@ -49,7 +91,7 @@ $(document).ready(function () {
                     <div class="row">
                         <div class="col-sm">
                             <div class="row mt-2" style="background-color: peachpuff;">
-                                <div class="col-sm mini-box" id="companyName">${dbCompanies[i].company_name}</div>
+                                <div class="col-sm mini-box" id="companyName">${sortedCompanies[i].company_name}</div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col-sm mini-box" id="rating"><i class="fa fa-star"> </i> <i class="fa fa-star">
@@ -59,7 +101,7 @@ $(document).ready(function () {
     
                     </div>
                 </div><div class="col-sm-2 ml-2" id="payBox">
-                <button type="button" class="btn btn-outline-success text-dark">$${dbCompanies[i].average_pay_per_hour.toFixed(2)}</button>
+                <button type="button" class="btn btn-outline-success text-dark">$${sortedCompanies[i].average_pay_per_hour.toFixed(2)}</button>
             </div>
             <div class="col-sm-2 ml-2" id="linkBox">
                 <div id="linkId">I am link</div>
